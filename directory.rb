6515efe5cc3f7167d0048880fa48c26eb57d6cae
Rob Oman <rob.oman@icloud.com>
2022-02-15 18:13:37 +0000

    Add save functionality to student list

--- directory.rb
+++ directory.rb
@@ -80,6 +80,7 @@ def print_menu
   #1. print the menu and ask the user what to do
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -91,11 +92,24 @@ def process(selection)
   when "2"
     #show the students
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
     puts "Please enter a valid number"
   end
 end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort], student[:height], student[:birth_country]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 
 interactive_menu
